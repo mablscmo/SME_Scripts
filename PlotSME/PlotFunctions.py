bcd92f2680f96f29fbea25af75878cb86f2df85d
mablscmo <m.ontelius@hotmail.com>
2020-03-23 01:22:07 +0100

    Add files via upload

---
+++ PlotSME/PlotFunctions.py
@@ -0,0 +1,142 @@
+"""
+-----------------------------------------
+Created on 2020-03-22
+author: Martin Montelius
+        nat14mmo@student.lu.se
+Version: 0.1
+-----------------------------------------
+Functions for the PlotAbundances code
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from PlotVariables import *
+
+
+
+def PickElement():
+    global element
+    while True:
+        try:
+                element = eval(input('Input element (q to quit): '))
+        except NameError:
+            print('Element not recogniced, check spelling or edit the code if we got a new element')
+            continue
+        break
+    if element in PlotInput:
+        print('Plotting {ele}...\n'.format(ele=element))
+    
+def PlotInternal(subject,row,rows):
+    if rows == 1:
+        for i in range(3):
+            ax[i].axvline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].axhline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].set_xlim([-2,1])
+            ax[i].set_ylim([-1,1])
+            ax[i].set_xlabel('[Fe/H]')
+            ax[i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=subject)].values,'.',color='crimson',label='IGRINS')
+            ax[i].plot(Comparison[i]['[Fe/H]'].values,Comparison[i]['{el}'.format(el=subject)].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+            ax[i].legend(loc='lower left',fancybox=True)    
+    else:
+        for i in range(3):
+            ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
+            ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
+            ax[row,i].set_xlim([-2,1])
+            ax[row,i].set_ylim([-1,1])
+            ax[row,i].set_xlabel('[Fe/H]')
+            ax[row,i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=subject)].values,'.',color='crimson',label='IGRINS')
+            ax[row,i].plot(Comparison[i]['[Fe/H]'].values,Comparison[i]['{el}'.format(el=subject)].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+            ax[row,i].legend(loc='lower left',fancybox=True)         
+
+def PlotElement(element, DATA, plotdir):
+    IGRINS_RESULTS, OPTICAL_RESULTS, IVALU_RESULTS, APOGEE_RESULTS = DATA
+    plt.ioff()
+    if element in Elements:
+        fig, ax = plt.subplots(1, 3, figsize=(21, 5))
+        for i in range(3):
+            ax[i].axvline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].axhline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].set_xlim([-2,1])
+            ax[i].set_ylim([-1,1])
+            ax[i].set_xlabel('[Fe/H]')
+            ax[i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=element)].values,'.',color='black',label='IGRINS')
+            ax[i].plot(DATA[i+1]['[Fe/H]'].values,DATA[i+1]['{el}'.format(el=element)].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+            ax[i].legend(loc='lower left',fancybox=True) 
+        ax[0].set_ylabel('[{ele}/Fe]'.format(ele=element))
+        plt.tight_layout()
+        plt.savefig(plotdir+element+'.pdf',dpi=500)   
+        plt.draw()
+        plt.show()
+        
+    if element == 'Alpha':
+        fig, ax = plt.subplots(len(AlphaElements), 3, figsize=(21, len(AlphaElements)*4))
+        for row in range(len(AlphaElements)):
+            for i in range(3):
+                ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].set_xlim([-2,1])
+                ax[row,i].set_ylim([-1,1])
+                ax[row,i].set_xlabel('[Fe/H]')
+                ax[row,i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=AlphaElements[row])].values,'.',color='black',label='IGRINS')
+                ax[row,i].plot(DATA[i+1]['[Fe/H]'].values,DATA[i+1]['{el}'.format(el=AlphaElements[row])].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+                ax[row,i].legend(loc='lower left',fancybox=True) 
+            ax[row,0].set_ylabel('[{el}/Fe]'.format(el=AlphaElements[row]))   
+        plt.tight_layout()
+        plt.savefig(plotdir+element+'.pdf',dpi=500)
+        plt.draw()
+        plt.show()
+        
+    #Need to measure more iron-peak elements before this code works
+    if element == 'Iron_peak':
+        fig, ax = plt.subplots(len(IronElements), 3, figsize=(21, len(IronElements)*4))
+        for row in range(len(IronElements)):
+            for i in range(3):
+                ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].set_xlim([-2,1])
+                ax[row,i].set_ylim([-1,1])
+                ax[row,i].set_xlabel('[Fe/H]')
+                ax[row,i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=IronElements[row])].values,'.',color='black',label='IGRINS')
+                ax[row,i].plot(DATA[i+1]['[Fe/H]'].values,DATA[i+1]['{el}'.format(el=IronElements[row])].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+                ax[row,i].legend(loc='lower left',fancybox=True) 
+            ax[row,0].set_ylabel('[{el}/Fe]'.format(el=IronElements[row]))   
+        plt.tight_layout()
+        plt.savefig(plotdir+element+'.pdf',dpi=500)
+        plt.draw()
+        plt.show()
+        
+    #Need to measure more neutron-capture elements before this code works
+    if element == 'Neutron_capture':
+        fig, ax = plt.subplots(len(NeutronElements), 3, figsize=(21, len(NeutronElements)*4))
+        for row in range(len(NeutronElements)):
+            for i in range(3):
+                ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
+                ax[row,i].set_xlim([-2,1])
+                ax[row,i].set_ylim([-1,1])
+                ax[row,i].set_xlabel('[Fe/H]')
+                ax[row,i].plot(IGRINS_RESULTS['[Fe/H]'].values,IGRINS_RESULTS['{el}'.format(el=NeutronElements[row])].values,'.',color='black',label='IGRINS')
+                ax[row,i].plot(DATA[i+1]['[Fe/H]'].values,DATA[i+1]['{el}'.format(el=NeutronElements[row])].values,'.',color=Colours[i],alpha=0.75,label=CompLabel[i])
+                ax[row,i].legend(loc='lower left',fancybox=True) 
+            ax[row,0].set_ylabel('[{el}/Fe]'.format(el=NeutronElements[row]))   
+        plt.tight_layout()
+        plt.savefig(plotdir+element+'.pdf',dpi=500)
+        plt.draw()
+        plt.show()
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PlotSME/PlotSME.py
@@ -0,0 +1,125 @@
+"""
+-----------------------------------------
+Created on 2020-03-13
+author: Martin Montelius
+        nat14mmo@student.lu.se
+Version: 0.2
+-----------------------------------------
+Plan:
+    Ok, så planen är att du ger åtminstånde två inputs i början: element och jämförelse. Kanske skippar att ge jämförelse som input.
+    
+    Element kan vara vilket som helst av de olika elementen eller en elementgrupp, alpha, iron-peak eller neutron capture (fråga efter fler grupper). 
+
+    Jämförelse parametern säger vad det ska plottas mot, kolumnerna ifall det är en grupplot. Jag tänker "optical", "Ivalu", "APOGEE", "all", "none",
+    men också en specialare, "other", som promptar att du ger ett annat element som du plottar mot. Kanske kan implementera som ett "compare" kommando.
+
+    Man kanske kan skriva nån kod som hittar rätt resultatfil, eller resultat filer för specifika linjer. Skriv det som ett update kommando,  
+    
+    Plan till diagnostic: ge input 'diagnostic' för allting, annars 'teff' 'logg' etc, sätt en if statement i PA som omdefinierar element till diagnostic
+    och frågar efter en extra element input. Kör en "If element in PlotInput: pl.PlotElement, If element in Diagnostics: pl.PlotDiagnostics"
+
+New in version 0.2:
+    Switched to fig, ax method of plotting.
+    Rewrote script into functions and changed the order so that multiple elements can be plotted without reloading data.
+        Note this works best with inline plotting, some trouble with interactive plots.
+    Split up the script into 3 files, PlotVariables with text info, PlotFunctions with functions.
+    
+Top priority:
+    Implement all optical stars as a background.
+    Create diagnostic plots, with difference to optical on y-axis and Teff/log g/[Fe/H]/vmic on the x-axis. Plan in plans.
+    Get the name of the element on the plots.
+    Create a all plots grop with coloured backgrounds.
+    
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import socket
+from PlotVariables import *
+import PlotFunctions as pl
+
+###These are just the plotting parameters, so they become in this document
+plt.rcParams['font.size']= 16
+plt.rcParams['xtick.minor.visible'], plt.rcParams['xtick.top'] = True,True
+plt.rcParams['ytick.minor.visible'], plt.rcParams['ytick.right'] = True,True
+plt.rcParams['xtick.direction'], plt.rcParams['ytick.direction'] = 'in','in'
+plt.rcParams['xtick.labelsize'] = plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['axes.labelsize'] = 11
+plt.rcParams['mathtext.fontset'] = 'cm'
+
+
+#Check computer to get directories and names
+Computer = socket.gethostname()
+if  Computer == 'ValorToMe':
+    ###Directories for my home computer
+    resdir = '/Users/monte/OneDrive - Lund University/Uni/Master/Scripts/'
+    homedir = '/Users/monte/OneDrive - Lund University/Uni/Master/Scripts/'
+    plotdir = '/Users/monte/OneDrive - Lund University/Uni/Master/Scripts/PlotFolder/'
+elif Computer == 'rap':
+    ###Directories for rap
+    resdir = '/home/martin/Analysis/smeresults/'
+    homedir = '/home/martin/'
+    plotdir = '/home/martin/AbundancePlots/'
+else:
+    print('Unfamiliar computer, please identify yourself by adding your directories to the code')
+
+
+#Read in results files
+IGRINS_RESULTS = pd.read_csv(resdir+'result_H_val08_ni_20200314_175239.txt', skiprows=[0,1,2], delim_whitespace=True, names=SME_Header)
+OPTICAL_RESULTS = pd.read_csv(resdir+'result_optical_start_H.txt', skiprows=[0,1,2,6], delim_whitespace=True, names=SME_Header)
+IVALU_DATA = pd.read_csv(homedir+'IVALU_Results.txt', skiprows=[0,1,2], delim_whitespace=True, names=SME_Header)
+APOGEE_RESULTS = pd.read_csv(homedir+'APOGEE_DATA.txt',names=APOGEE_Header,delim_whitespace=True)
+
+MY2MASS = IGRINS_RESULTS['2MASS'].values
+
+IVALU_RESULTS = IVALU_DATA[IVALU_DATA['2MASS'].isin(list(MY2MASS))]
+
+DATA = [IGRINS_RESULTS, OPTICAL_RESULTS, IVALU_RESULTS, APOGEE_RESULTS]
+
+
+
+while True:
+    pl.PickElement()
+    element = pl.element
+    if element == 'quit':
+        print('Plotting stopped')
+        break  
+    pl.PlotElement(element, DATA, plotdir)
+    print('Plotted {el}'.format(el=element))
+    continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
