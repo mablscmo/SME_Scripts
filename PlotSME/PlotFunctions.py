3d955474a590c4752347b2d45796490d93c22862
Martin Montelius <m.ontelius@hotmail.com>
2020-03-24 11:24:22 +0100

    Update to 0.3

--- PlotSME/PlotFunctions.py
+++ PlotSME/PlotFunctions.py
@@ -2,7 +2,8 @@
 -----------------------------------------
 Created on 2020-03-22
 author: Martin Montelius
-Version: 0.1
+        nat14mmo@student.lu.se
+Version: 0.3
 -----------------------------------------
 Functions for the PlotAbundances code
 """
@@ -49,10 +50,11 @@ def PlotInternal(subject,row,rows):
             ax[row,i].legend(loc='lower left',fancybox=True)         
 
 def PlotElement(element, DATA, plotdir):
-    IGRINS_RESULTS, OPTICAL_RESULTS, IVALU_RESULTS, APOGEE_RESULTS = DATA
+    IGRINS_RESULTS, OPTICAL_RESULTS, IVALU_RESULTS, APOGEE_RESULTS, OPTICAL_COMPLETE = DATA
     plt.ioff()
     if element in Elements:
         fig, ax = plt.subplots(1, 3, figsize=(21, 5))
+        ax[0].plot(OPTICAL_COMPLETE['[Fe/H]'].values,OPTICAL_COMPLETE['{el}'.format(el=element)].values,'.',color='silver',label='Complete optical')
         for i in range(3):
             ax[i].axvline(0,0,1, color='0.75', linestyle='dashed')
             ax[i].axhline(0,0,1, color='0.75', linestyle='dashed')
@@ -71,6 +73,7 @@ def PlotElement(element, DATA, plotdir):
     if element == 'Alpha':
         fig, ax = plt.subplots(len(AlphaElements), 3, figsize=(21, len(AlphaElements)*4))
         for row in range(len(AlphaElements)):
+            ax[row,0].plot(OPTICAL_COMPLETE['[Fe/H]'].values,OPTICAL_COMPLETE['{el}'.format(el=AlphaElements[row])].values,'.',color='silver',label='Complete optical')
             for i in range(3):
                 ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
                 ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
@@ -90,6 +93,7 @@ def PlotElement(element, DATA, plotdir):
     if element == 'Iron_peak':
         fig, ax = plt.subplots(len(IronElements), 3, figsize=(21, len(IronElements)*4))
         for row in range(len(IronElements)):
+            ax[0].plot(OPTICAL_COMPLETE['[Fe/H]'].values,OPTICAL_COMPLETE['{el}'.format(el=IronElements[row])].values,'.',color='silver',label='Complete optical')
             for i in range(3):
                 ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
                 ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
@@ -109,6 +113,7 @@ def PlotElement(element, DATA, plotdir):
     if element == 'Neutron_capture':
         fig, ax = plt.subplots(len(NeutronElements), 3, figsize=(21, len(NeutronElements)*4))
         for row in range(len(NeutronElements)):
+            ax[0].plot(OPTICAL_COMPLETE['[Fe/H]'].values,OPTICAL_COMPLETE['{el}'.format(el=NeutronElements[row])].values,'.',color='silver',label='Complete optical')
             for i in range(3):
                 ax[row,i,].axvline(0,0,1, color='0.75', linestyle='dashed')
                 ax[row,i].axhline(0,0,1, color='0.75', linestyle='dashed')
@@ -126,16 +131,43 @@ def PlotElement(element, DATA, plotdir):
 
     
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+def PlotDiagnostics(element, diag, DATA, DIF_DATA, plotdir):
+    IGRINS_RESULTS, OPTICAL_RESULTS, IVALU_RESULTS, APOGEE_RESULTS, OPTICAL_COMPLETE = DATA
+    
+    plt.ioff()
+    
+    if diag == 'diagnostics':
+        fig, ax = plt.subplots(1, 4, figsize=(26, 5))
+        for i in range(4):
+            # ax[i].axvline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].axhline(0,0,1, color='0.75', linestyle='dashed')
+            ax[i].set_xlim(DiagLimits[i])
+            ax[i].set_ylim([-1,1])
+            ax[i].set_xlabel(Diagnostics[i+1])
+            ax[i].plot(IGRINS_RESULTS[Diagnostics[i+1]].values,DIF_DATA['{el}'.format(el=element)].values,'.',markersize=10,color=DiagColours[i])
+        ax[0].set_ylabel(r'[{ele}/Fe]$_{{IGRINS}}$-[{ele}/Fe]$_{{OPTICAL}}$'.format(ele=element))
+        plt.tight_layout()
+        plt.savefig(plotdir+diag+'_'+element+'.pdf',dpi=500)   
+        plt.draw()
+        plt.show()
+    elif diag == 'Teff':
+        i = 0
+    elif diag == 'logg':
+        i = 1
+    elif diag == 'vmic':
+        i = 2
+    elif diag == '[Fe/H]':
+        i = 3
+    if diag != 'diagnostics':
+        plt.figure(figsize=(9,5))
+        # ax[i].axvline(0,0,1, color='0.75', linestyle='dashed')
+        plt.axhline(0,0,1, color='0.75', linestyle='dashed')
+        plt.xlim(DiagLimits[i])
+        plt.ylim([-1,1])
+        plt.xlabel(Diagnostics[i+1])
+        plt.ylabel(r'[{ele}/Fe]$_{{IGRINS}}$-[{ele}/Fe]$_{{OPTICAL}}$'.format(ele=element))
+        plt.plot(IGRINS_RESULTS[Diagnostics[i+1]].values,DIF_DATA['{el}'.format(el=element)].values,'.',markersize=10,color=DiagColours[i])
+        plt.tight_layout()
+        plt.savefig(plotdir+diag+'_'+element+'.pdf',dpi=500)   
+        plt.draw()
+        plt.show()
