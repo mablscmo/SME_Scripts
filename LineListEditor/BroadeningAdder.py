6e0cf1a967babbccb93691dec59548400022e7a6
Martin Montelius <m.ontelius@hotmail.com>
2020-04-09 02:47:51 +0200

    Efficiency update
    Updated file writing system so that it now is slightly less terrible. Working on a better rounding system.

--- LineListEditor/BroadeningAdder.py
+++ LineListEditor/BroadeningAdder.py
@@ -26,12 +26,13 @@ LineList = pd.read_csv('LineList.dat', index_col=False, names=ListHeader, skipro
 BroadeningHeader = ['Element','Ion','Lambda','Exc','loggf','DampGrade','DampSig','DampVel','W','Wobs','error','abund','not sure','Waals','CombName']
 BroadeningData = pd.read_csv('s4000_g+1.5_m1.0_t02_mc_z+0.00_a+0.00_c-0.13_n+0.31_o+0.00_r+0.00_s+0.00_H_abo.eqw', delim_whitespace=True, index_col=False, names=BroadeningHeader, skiprows=1)
 BroadeningData['Waals'] = np.floor(BroadeningData['DampSig'])+BroadeningData['DampVel'] #Combines values to SME format
-BroadeningData['CombName'] = "'" + BroadeningData['Element'] + ' ' + BroadeningData["Ion"].map(str) + "'" #Combines names to mimic VALD format
+BroadeningData['CombName'] = "'" + BroadeningData['Element'] + ' ' + BroadeningData["Ion"].map(str) + "'" #Combines names to VALD format
 BroadeningValues = BroadeningData.loc[BroadeningData.Waals != 0] #Limits the document to where there is broadening data
 BVindex = BroadeningValues.index #Index of broadening values, this is probably stupid
 
 
 "____________________________Find lines in linelist and insert broadening values____________________________"
+
 for i in range(len(BroadeningValues)): 
     try:#__________________wavelength where Linelist name == Broadeninglist name_____________________with wavelength________________________closest to the one in the linelist
         llindex = LineList['LambdaAir'][LineList['EleIon'] == BroadeningValues['CombName'].values[i]].sub(BroadeningValues['Lambda'].values[i]).abs().idxmin()
@@ -51,17 +52,18 @@ Other = open("LineList.dat","r")
 #Keep track of where in the imported dataframe you are
 count=0
 
-#len(LineList)*4+3 gives the total length of the linelist file, remember LineList is only the data
-for i in range(len(LineList)*4+3):
+progress = 0
+for i in range(TotLength):
     if logic(i) == False:
         Data = LineList.iloc[count]
         
         #format() creates the appropriate number of decimals, better than round()
-        element = Data[0]
+        element = Data[0] + ','
+        lam=format(Data[1],'.3f')
         gf = format(Data[2],'.3f')
         El = format(Data[3],'.4f')
         Jlow = Data[4]
-        Eu = format(Data[5],'.4f')
+        Eu = format(Data[5],'.4f') 
         Jhigh = Data[6]
         LaL = format(Data[7],'.3f')
         LaU = format(Data[8],'.3f')
@@ -71,8 +73,8 @@ for i in range(len(LineList)*4+3):
         Waals = format(Data[12],'.3f')
         CD = format(Data[13],'.3f')
         
-        NewFile.write('''{ele},{wsp1}{lam},{wsp2}{gf},{wsp3}{El},{wsp4}{Jlow},{wsp5}{Eu},{wsp6}{Jhigh},{wsp7}{LaL},{wsp8}{LaU},{wsp9}{LaM},{wsp10}{Rad},{wsp11}{Stark},{wsp12}{Waals}, {CD},
-'''.format(ele = element, wsp1=' '*(13-len(element)), lam=format(Data[1],'.3f'), wsp2=' '*(8-len(str(gf))), gf=gf, wsp3=' '*(8-len(str(El))),El=El, wsp4=' '*(5-len(str(Jlow)))  ,Jlow=Jlow, wsp5=' '*(8-len(str(Eu))), Eu=Eu, wsp6=' '*(5-len(str(Jhigh)))  ,Jhigh=Jhigh, wsp7=' '*(7-len(str(LaL))), LaL=LaL, wsp8=' '*(7-len(str(LaU))), LaU=LaU, wsp9=' '*(7-len(str(LaM))), LaM=LaM, wsp10=' '*(6-len(str(Rad))), Rad=Rad, wsp11=' '*(6-len(str(Stark))), Stark=Stark, wsp12=' '*(8-len(str(Waals))), Waals=Waals, CD = CD))
+        NewFile.write('''{0: <14}{1},{2: >8},{3: >8},{4: >5},{5: >8},{6: >5},{7: >7},{8: >7},{9: >7},{10: >6},{11: >6},{12: >8},{13: >6},
+'''.format(element, lam, gf, El, Jlow, Eu, Jhigh, LaL, LaU, LaM, Rad, Stark, Waals, CD))
         
         #Uses count instead of i as we only take a value from LineList every fourth iteration
         count += 1
@@ -82,7 +84,10 @@ for i in range(len(LineList)*4+3):
     else:
         #Write the unchanged lines from the old linelist to the new one
         NewFile.write(Other.readline())
-
+    
+    if round(10*(1+i)/TotLength) != progress:
+        progress = round(10*(1+i)/TotLength)
+        print('{nr}% '.format(nr=progress*10))
 #Close files to save the changes
 Other.close()
 NewFile.close()
