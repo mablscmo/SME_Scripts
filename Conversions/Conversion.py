48602a5563cd3294306b9d3b716fcb3007d63726
Martin Montelius <m.ontelius@hotmail.com>
2021-01-02 00:07:49 +0100

    Update to 0.3
    Implemented command line functionality with conversion(), works better than expected.
    Added better unit handling for hz_to_wl and wl_to_hz.
    Cleaned up some formatting.

--- Conversions/Conversion.py
+++ Conversions/Conversion.py
@@ -1,6 +1,6 @@
 """
 -----------------------------------------
-Created on 2020-04-158
+Created on 2020-04-15
 author: Martin Montelius
 Version: 0.2.1
 -----------------------------------------
@@ -20,17 +20,15 @@ edlen66 is the one VALD uses.
 
 inv_to_ev converts energylevels between inverse centimeters and electronvolts.
 
-New in version 0.2:
-    New function conversion(), takes From and To as arguments and lets you write in numbers to your hearts content, converting them to whatever you
-    want (as long as it is implemented). Can also take number of decimals as input, default is 3, and the vac to air variables just like the 
-    regular functions. Give it q to quit. 
-    Also implemented electronvolt to inverse centimeters function.
-
-    0.2.1:
-    Implemented eV/inv cm to vac/air/nr. The conversion() implementation works but is sort of wonky, giving input as a tuple, list or just a comma
-    between two numbers works fine. 
+New in version 0.3:
+    Implemented command line functionality with conversion(), works better than expected.
+    Added better unit handling for hz_to_wl and wl_to_hz
+    Cleaned up some formatting
     
 
+To do list:
+    
+    Import new air2vac, vac2air parameters from PySME's util.py. Not sure where they get them, Piskunov wrote it though.
 """
 import numpy as np
 
@@ -186,13 +184,42 @@ def inv_to_nr(lower,upper,c=c,h=h):
         upper = np.array(upper)
     return(vac_to_nr(h*c*10**10/(inv_to_ev(upper)-inv_to_ev(lower))))
 
+"______________________________________________Frequency to wl______________________________________________"
 
+def hz_to_wl(hz,prefix=None,angstrom=False,c=c):
+    '''Default takes Hz and gives meters. Remember that differences need to be
+    calculated from a set value.'''
+    if isinstance(hz,(list,tuple)):
+        hz = np.array(hz)
+    if prefix != None:
+        prefix = prefix.capitalize()
+        if prefix == 'K':
+            hz = hz*10**3
+        elif prefix == 'M':
+            hz = hz*10**6
+        elif prefix == 'G':
+            hz = hz*10**9
+        elif prefix == 'T':
+            hz = hz*10**12
+    if angstrom != False:
+        c = c*10**10            
+    return(c/hz)
+
+def wl_to_hz(wl,angstrom=False,c=c):
+    '''Default takes Hz and gives meters. Remember that differences need to be
+    calculated from a set value.'''
+    if isinstance(wl,(list,tuple)):
+        wl = np.array(wl)
+    if angstrom != False:
+        c = c*10**10           
+    return(c/wl)
 
 "____________________________________________CONVERSION FUNCTION____________________________________________"
-air, vac, nr, wl, inv, ev, erg, q = 'air', 'vac', 'nr', 'wl', 'inv', 'ev', 'erg', 'quit'
-Air, Vac, Nr, Wl, Inv, eV, Erg, Q = 'air', 'vac', 'nr', 'wl', 'inv', 'ev', 'erg', 'quit'
+air, vac, nr, wl, inv, ev, erg, hz, q, k, m, g, t = 'air', 'vac', 'nr', 'wl', 'inv', 'ev', 'erg', 'hz', 'quit', 'K', 'M', 'G', 'T'
+Air, Vac, Nr, Wl, Inv, eV, Erg, Hz, Q, K, M, G, T = 'air', 'vac', 'nr', 'wl', 'inv', 'ev', 'erg', 'hz', 'quit', 'K', 'M', 'G', 'T'
 
 def conversion(From, To, dec=3, vta_constants=ciddor96):
+    print('\n')
     if (From == 'air') & (To == 'vac'):
         def converter(number):
             return(print(round(float(air_to_vac(number)),dec)))
@@ -256,20 +283,71 @@ def conversion(From, To, dec=3, vta_constants=ciddor96):
         print('Give input as: upper,lower')
         def converter(number):
             return(print(round(float(inv_to_nr(number[0],number[1])),dec)))
+    elif (From == 'hz') & (To == 'wl'):
+        print('Give input as: frequency,prefix')
+        def converter(number):
+            wl = float(hz_to_wl(number[0],number[1]))
+            if np.all(wl >= 10**3):
+                pre, div  = 'km', 10**3
+            elif np.all(wl >= 10**0) & np.all(wl < 10**3):
+                pre, div = 'm', 10**0
+            elif np.all(wl >= 10**-2) & np.all(wl < 10**0):
+                pre, div = 'cm', 10**-2
+            elif np.all(wl >= 10**-4) & np.all(wl < 10**-2):
+                pre, div  = 'mm', 10**-3
+            elif np.all(wl >= 10**-6) & np.all(wl < 10**-4):
+                pre, div  = 'um', 10**-6
+            elif np.all(hz <= 10**-6):
+                pre, div = 'Å', 10**-10
+            else:
+                pre, div = '',1
+            return(print(f'{round(wl/div,dec)} {pre}'))            
+    elif (From == 'wl') & (To == 'hz'):
+        def converter(number):
+            hz = float(wl_to_hz(number,angstrom=False))
+            if np.all(hz >= 10**3) & np.all(hz < 10**6):
+                pre, div = 'K', 10**3
+            elif np.all(hz >= 10**6) & np.all(hz < 10**9):
+                pre, div = 'M', 10**6
+            elif np.all(hz >= 10**9) & np.all(hz < 10**12):
+                pre, div  = 'G', 10**9
+            elif np.all(hz >= 10**12):
+                pre, div = 'T', 10**12
+            else:
+                pre, div = '',1
+            return(print(f'{round(hz/div,dec)} {pre}Hz'))
     else:
-        print('Conversion from {} to {} not yet implemented'.format(From,To))
-        raise SystemExit('Please try again')
+        print('\nConversion from {} to {} not yet implemented'.format(From,To))
+        raise SystemExit('Please try again\n')
     global number
-    print("Input 'q' to quit")
+    print("Input 'q' to quit\n")
     while True:
         try:
             number = eval(input('{} to {}: '.format(From,To)))
         except NameError:
-            print('Input not recogniced')
+            print('Input not recogniced\n')
+            continue
+        except SyntaxError:
+            print('Input not recogniced\n')
             continue
         if number == 'quit':
-            print('Conversion stopped')
+            # This could be done with raise
+            print('Conversion stopped\n')
             break  
         else:
             converter(number)
+            print('\n')
         continue
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+    
+    if len(sys.argv) > 1:
+        parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,description='''Convert between different units, actual python file contains functions,\nrunning in command line is easier for quick conversions.\n\n------------------------------------------------------------\n                    IMPLEMENTED COMMANDS                    \n------------------------------------------------------------\nair: wavelength in air\nvac: wavelength in vacuum\nwl:  vacuum below 2000 Å and above 20000 Å, air otherwise\nnr:  wavenumber\ninv: energy in inverse centimeters\nev:  energy in electronvolts\nhz:  frequency in Hertz, with support for different prefixes\n''')
+        parser.add_argument("From", type=str, help="convert from, choose from implemented commands")
+        parser.add_argument("To", type=str, help="convert to, choose from implemented commands")
+        parser.add_argument("Dec", nargs='?', type=int, help="number of decimal places shown", default=3)
+    
+        args = parser.parse_args()
+        answer = conversion(args.From,args.To,args.Dec)
